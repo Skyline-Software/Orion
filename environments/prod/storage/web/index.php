e54073e9ab67932d6b0c38cf00b8529404b85170
Mopkau <yiiframe@yandex.ru>
2019-03-13 22:59:13 +0200

    upd

--- environments/prod/storage/web/index.php
+++ environments/prod/storage/web/index.php
@@ -1,14 +1,17 @@
 <?php
-// Composer
-require(__DIR__ . '/../../vendor/autoload.php');
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
-// Yii
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../common/config/bootstrap.php';
+require __DIR__ . '/../config/bootstrap.php';
 
-// Bootstrap
-require(__DIR__ . '/../../common/config/bootstrap.php');
-
-// App Config
-$config = require(__DIR__ . '/../config/base.php');
+$config = yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../config/main-local.php'
+);
 
 (new yii\web\Application($config))->run();
