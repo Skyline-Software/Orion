3d6412aa2ac2d948ea810c81dfb303e206045739
Mopkau <yiiframe@yandex.ru>
2019-04-03 16:48:35 +0300

    upd

--- core/forms/manage/user/AgencyAdminForm.php
+++ core/forms/manage/user/AgencyAdminForm.php
@@ -43,7 +43,7 @@ class AgencyAdminForm extends CompositeForm
             $this->status = $user->status;
             $this->profile = new ProfileForm($user->profile);
             $this->photo = new ImageForm('PhotoForm', new Image($user->profile->photo));
-            $this->agencies = new MultipleInputForm('AgenciesForm', new Rows($user->getAgencyAssn()->where(['in','agency_id',AgencyHelper::getAllowedAgenciesIds()])));
+            $this->agencies = new MultipleInputForm('AgenciesForm', new Rows($user->getAgencyAssn()->where(['in','user_agency_assn.agency_id',AgencyHelper::getAllowedAgenciesIds()])));
 
             $this->_user = $user;
         }else{
