5ba3a998c2d0e0abedb6f6a0010bdf9d533f9890
Rinat Shaykhutdinov <rinatshay@protonmail.com>
2019-07-23 14:45:15 +0300

    Update deployment

--- common/config/main.php
+++ common/config/main.php
@@ -15,13 +15,6 @@ return [
             'cachePath' => '@runtime/glide',
             'signKey' => false // "false" if you do not want to use HTTP signatures
         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=sugoyznuhswf4z5j;port=3306',
-            'username' => 'pgw2ni8kx3ihi26q',
-            'password' => 'znd7087imsarb744',
-            'charset' => 'utf8',
-        ],
         'fcm' => [
             'class' => 'understeam\fcm\Client',
             'apiKey' => 'AAAAEPFFJ_o:APA91bEoU3Vz1z8-bH3wJ9hFrjp724Az4RCJq4CvNQPwJ1BTO1xLN6DMa9DAG48nKdHwXzvZnWIkwhzQXUDXElTrsxgwQOiG_7nrb-B9eoqg4ZmaymgV_obeR_uspwqmNn75Iw0ujiS9',
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=op',
-            'username' => 'op',
-            'password' => '0ri0nSQLpazz',
+            'dsn' => 'mysql:host=zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=sugoyznuhswf4z5j;port=3306',
+            'username' => 'pgw2ni8kx3ihi26q',
+            'password' => 'znd7087imsarb744',
             'charset' => 'utf8',
         ],
         'mailer' => [
