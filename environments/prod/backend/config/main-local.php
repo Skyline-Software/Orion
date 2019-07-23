2a65e02532b3b1e8903f98693eeee2bcd8c5e502
Rinat Shaykhutdinov <rinatshay@protonmail.com>
2019-07-23 15:22:11 +0300

    Update main-local.php

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -1,5 +1,18 @@
 <?php
 
-$config = [];
-
-return $config;
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=sugoyznuhswf4z5j;port=3306',
+            'username' => 'pgw2ni8kx3ihi26q',
+            'password' => 'znd7087imsarb744',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'useFileTransport' => true,
+        ],
+    ],
+];
